3c6624d1d8f4f254ef392b733a3c869b2a223e89
helltractor <helltractor@users.noreply.github.com>
2025-05-11 07:38:27 +0000

    deploy: ebd82aed0c32a4419e2f9e85c65b3665bb96404d

---
+++ assets/back.D_kLyZ6O.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.CTK7L64s.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
